0c5b2c55504373d1adc62bd2a523e18d34d418ad
Júlio Nienow <julio.nienow@operacao.rcadigital.com.br>
2019-12-04 13:43:10 -0300

    adicionado script para inicializacao do projeto / adicionado alert com to dos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  ngOnInit() {
+    alert('To dos\n- Loader\n- Particles\n- Lugares que trabalhei com timeline com parallax\n- Projetos que ja desenvolvi\n- Animações de compoentes de entrada e saida e botoes com css\n- Integração com redes socaiis?\n - Referencias layout (falar com rambo)\n - http://www.sharpeart.com/designer/\n - https://startbootstrap.com/template-categories/all/\n - https://aviebrantz.com.br/projects\n- Deploy automatizado que vini indicou now serverless application\n- Angular universal SEO\n - Colocar favicon');
+  }
 }
